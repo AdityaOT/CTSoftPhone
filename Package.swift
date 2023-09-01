011efd6a78a8161c2fb21ef630c774ff377f04f2
AdityaOT <143172434+AdityaOT@users.noreply.github.com>
2023-09-01 12:32:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CTSoftPhone",
-            url: "https://github.com/AdityaOT/SipFramework/releases/download/1.0.0/CTSoftPhone.xcframework.zip",
-            checksum: "5ce3b14b3efd71bc1d625ee96fa121ef3cd29ad1bf1168451ac2f3033315b889"
+            url: "https://github.com/AdityaOT/CTSoftPhone/releases/download/1.0.0/CTSoftPhone-1.0.0.zip",
+            checksum: "3e081d900112e4d04618cdf310f0750777ce7ba1e72db256b155b61324887b9c"
         )
     ]
 )
